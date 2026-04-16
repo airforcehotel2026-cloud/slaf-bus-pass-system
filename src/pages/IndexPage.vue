62aa94aedaa877eb329ab4e129a99a2f10061a80
Deepal Jayasekara <airforcehotel2026@gmail.com>
2026-04-16 15:02:37 +0530

    bakground fix

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -49,8 +49,8 @@
 
 // Hero Section styling
 .hero-section {
-  height: 100vh; // Full screen now since there's no content below it
-  background-image: url('hero-aircraft.png');
+  height: 100vh;
+  background-image: url('/hero-aircraft.png');
   background-size: cover;
   background-position: center;
   position: relative;
